18ed3766020669b7d04358148411a7c161af71eb
Michael James Hon <mhon2014@my.fit.edu>
2019-04-14 17:28:44 -0400

    changed c and h

--- c/hangmanTrie.c
+++ c/hangmanTrie.c
@@ -17,7 +17,7 @@ typedef struct node{
   char letter;
 
   struct node *parent,
-              *next,
+            //   *next, //if using linked list
             //   *children;
               *children[ALPHABET_SIZE];
   unsigned int depth;
@@ -51,38 +51,46 @@ Node *node(){
     Node *newNode = (Node*)malloc(sizeof(Node));
     // newNode->letter = NULL;
     newNode->parent = NULL;
-    newNode->next = NULL;
     // newNode->children = NULL;
     newNode->depth = 0;
     newNode->EndOfWord = false;
     for(int i = 0; i < ALPHABET_SIZE; i++){
         newNode->children[i] = NULL;
-    } //I don't like this;
+    } //I don't like this AT ALL
     return newNode;
 }
+
 
 Trie *initTrie(){ //initialize the trie.
     Trie *newTrie = (Trie*)malloc(sizeof(Trie));
     newTrie->root = node();
     newTrie->nodeCount = 1;
+    return newTrie;
 }
 
 void insert(Trie *arg_trie, char *arg_word){
 
     Node *iterator = arg_trie->root; 
+    // printf("\nlength: %lu - ASCII value: %d - word: %s \n", strlen(arg_word)-2, arg_word[0], arg_word);
 
+    // return;
     //refactor to make it look nice pls
     for(int i = 0;i < strlen(arg_word)-2; ++i){
-
         //if empty
         //oh god this is disgusting.....
         if(!iterator->children[INDEX(arg_word[i])]){
             //initialise the new node
+            // printf("parent: %c - depth: %d\n", iterator->letter, iterator->depth);
             Node *newNode = node();
+            newNode->parent = iterator;
             newNode->letter = arg_word[i];
+            newNode->depth = i+1;
+            iterator->children[INDEX(arg_word[i])] = newNode;
+            arg_trie->nodeCount++;
         }
-        iterator->children[INDEX(arg_word[i])];
-        //some other conditions.....
+        iterator = iterator->children[INDEX(arg_word[i])];
+        // printf("letter: %c\n", iterator->letter);
+
         iterator->EndOfWord = true;
     }
 }
