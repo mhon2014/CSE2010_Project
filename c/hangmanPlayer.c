398498ff2206a00c0fafa23f5dab5a3665cb9b3b
sarends <saraharends614@gmail.com>
2019-04-16 11:17:33 -0400

    moved funct to c file from header

--- c/hangmanPlayer.c
+++ c/hangmanPlayer.c
@@ -67,7 +67,6 @@ char guess_hangman_player(char* current_word, bool is_new_word) {
   // variable declarations
   char guess = ' ';
   
-
   // reset guesses if new word
   if (is_new_word) {
     for(int i = 0; i < ALPHABET_SIZE; i++){
@@ -77,6 +76,8 @@ char guess_hangman_player(char* current_word, bool is_new_word) {
 
 
   // formulate next guess
+
+
 
   // update guess_letters
 
@@ -107,3 +108,71 @@ void feedback_hangman_player(bool is_correct_guess, char* current_word) {
 
 } // end feedback
 
+// initialize and return a new alpha node
+AlphaNode* new_alpha_node() {
+
+    AlphaNode* new_node = (AlphaNode*) malloc(sizeof(AlphaNode)); 
+
+    // initialize parent and children pointers
+    new_node->parent = NULL;
+    for(int i = 0; i < ALPHABET_SIZE; i++){
+        new_node->children[i] = NULL;
+    } 
+
+    // initialize other node fields
+    new_node->depth = 0;
+    new_node->is_candidate = true;
+    new_node->end_of_word = false;
+
+    return new_node;
+
+}
+
+//initialize and return a new trie
+Trie *init_trie() { 
+
+    Trie *newTrie = (Trie*)malloc(sizeof(Trie));
+
+    newTrie->root = new_alpha_node();  // initialize root node
+    newTrie->root->letter = '#';       // special character for root
+    newTrie->nodeCount = 1;            // number of nodes in tree
+
+    return newTrie;
+
+} // end init_trie
+
+// inserts given word into given trie
+void insert(Trie *arg_trie, char *arg_word) {
+
+    // variable declarations
+    AlphaNode* new_node;                  // pointer for mallocing new nodes
+    AlphaNode* current_node = arg_trie->root; // node pointer for traversal
+    char current_letter;                  // stores letter as word is traversed
+
+    // for each character in the word
+    for (int i = 0; i < strlen(arg_word)-2; i++) {
+
+        current_letter = arg_word[i];
+
+        //if current node doesn't have this letter as child, initialize it
+        if( (current_node->children[CHAR_TO_INDEX(current_letter)]) == NULL) {
+            
+            //initialise a new node
+            new_node = new_alpha_node(); // initialize and allocate new node
+            new_node->parent = current_node; // make it child of current node
+            new_node->letter = current_letter; 
+            new_node->depth = i+1;  // child level is one below current level
+            current_node->children[CHAR_TO_INDEX(current_letter)] = new_node; // make parent point to new node
+            arg_trie->nodeCount++; // increment size of trie
+  
+        } // end if
+
+        current_node = current_node->children[CHAR_TO_INDEX(current_letter)]; // move to next level in trie
+    
+    } // end for loop
+
+    current_node->end_of_word = true; // mark node of last letter as end_of_word
+    
+    return;
+
+} // end insert function
--- c/hangmanTrie.h
+++ c/hangmanTrie.h
@@ -18,14 +18,7 @@ typedef struct AlphaNode {
 
 } AlphaNode;
 
-// node in a linked list of pointers
-typedef struct PtrNode {
-
-    AlphaNode* alpha_node_ptr;
-    struct PtrNode* next;
-
- } PtrNode;
-
+// trie data type
 typedef struct {
 
     AlphaNode *root;
@@ -33,70 +26,15 @@ typedef struct {
 
 } Trie;
 
-// initialize and return a new alpha node
-AlphaNode* new_alpha_node() {
+// node in list, storing pointer addresses
+typedef struct PtrNode {
 
-    AlphaNode* new_node = (AlphaNode*) malloc(sizeof(AlphaNode)); 
+    AlphaNode* alpha_node_ptr;
+    struct PtrNode* next;
 
-    // initialize parent and children pointers
-    new_node->parent = NULL;
-    for(int i = 0; i < ALPHABET_SIZE; i++){
-        new_node->children[i] = NULL;
-    } 
+ } PtrNode;
 
-    // initialize other node fields
-    new_node->depth = 0;
-    new_node->is_candidate = true;
-    new_node->end_of_word = false;
-
-    return new_node;
-
-}
-
-//initialize and return a new trie
-Trie *init_trie() { 
-
-    Trie *newTrie = (Trie*)malloc(sizeof(Trie));
-
-    newTrie->root = new_alpha_node();  // initialize root node
-    newTrie->root->letter = '#';       // special character for root
-    newTrie->nodeCount = 1;            // number of nodes in tree
-
-    return newTrie;
-}
-
-// inserts given word into given trie
-void insert(Trie *arg_trie, char *arg_word) {
-
-    // variable declarations
-    AlphaNode* new_node;                  // pointer for mallocing new nodes
-    AlphaNode* current_node = arg_trie->root; // node pointer for traversal
-    char current_letter;                  // stores letter as word is traversed
-
-    // for each character in the word
-    for (int i = 0; i < strlen(arg_word)-2; i++) {
-
-        current_letter = arg_word[i];
-
-        //if current node doesn't have this letter as child, initialize it
-        if( (current_node->children[CHAR_TO_INDEX(current_letter)]) == NULL) {
-            
-            //initialise a new node
-            new_node = new_alpha_node(); // initialize and allocate new node
-            new_node->parent = current_node; // make it child of current node
-            new_node->letter = current_letter; 
-            new_node->depth = i+1;  // child level is one below current level
-            current_node->children[CHAR_TO_INDEX(current_letter)] = new_node; // make parent point to new node
-            arg_trie->nodeCount++; // increment size of trie
-  
-        } // end if
-
-        current_node = current_node->children[CHAR_TO_INDEX(current_letter)]; // move to next level in trie
-    
-    } // end for loop
-
-    current_node->end_of_word = true; // mark node of last letter as end_of_word
-    
-    return;
-
-} // end insert function
+// function declarations
+AlphaNode* new_alpha_node(); // initialize and return a new alpha node
+Trie *init_trie(); //initialize and return a new trie
+void insert(Trie *arg_trie, char *arg_word); // inserts given word into given trie
